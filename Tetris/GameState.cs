63578b2aefef584454bfa8cfed270a12427c5251
Jorge A. Lopez <Jorgelopez5252@gmail.com>
2026-03-13 22:43:41 -0700

    GameState.cs added

--- Tetris/GameState.cs
+++ Tetris/GameState.cs
@@ -2,7 +2,8 @@
 {
     public class GameState
     {
-        private Block currentBlock;
+        //private Block currentBlock;
+        private Block currentBlock = null!;
 
         public Block CurrentBlock
         {
@@ -19,13 +20,102 @@
         public bool GameOver { get; private set; }
 
 
-        public GameState() {
+        public GameState()
+        {
             GameGrid = new GameGrid(22, 10);
             BlockQueue = new BlockQueue();
             CurrentBlock = BlockQueue.GetAndUpdate();
         }
 
-        
 
+        private bool BlockFits()
+        {
+            foreach (Position p in CurrentBlock.TilesPositions())
+            {
+                if (!GameGrid.IsEmpty(p.Row, p.Column))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public void RotateBlockCW()
+        {
+            CurrentBlock.RotateCW();
+
+            if (!BlockFits())
+            {
+                CurrentBlock.RotateCCW();
+            }
+        }
+
+        public void RotateBlockCCW()
+        {
+            CurrentBlock.RotateCCW();
+
+            if (!BlockFits())
+            {
+                CurrentBlock.RotateCW();
+            }
+        }
+
+        public void MoveBlockLeft()
+        {
+            CurrentBlock.Move(0, -1);
+
+            if (!BlockFits())
+            {
+                CurrentBlock.Move(0, 1);
+            }
+        }
+
+        public void MoveBlockRight()
+        {
+            CurrentBlock.Move(0, 1);
+
+            if (!BlockFits())
+            {
+                CurrentBlock.Move(0, -1);
+            }
+        }
+
+        private bool IsGameOver()
+        {
+            return !(GameGrid.IsRowEmpty(0) && GameGrid.IsRowEmpty(1));
+        }
+
+        private void PlaceBlock()
+        {
+            foreach (Position p in CurrentBlock.TilesPositions())
+            {
+                GameGrid[p.Row, p.Column] = CurrentBlock.Id;
+            }
+
+            GameGrid.ClearFullRows();
+
+            if (IsGameOver())
+            {
+                GameOver = true;
+            }
+            else
+            {
+                CurrentBlock = BlockQueue.GetAndUpdate();
+            }
+        }
+
+        public void MoveBlockDown()
+        {
+
+            if (GameOver) return;
+            CurrentBlock.Move(1, 0);
+
+            if (!BlockFits())
+            {
+                CurrentBlock.Move(-1, 0);
+                PlaceBlock();
+            }
+        }
     }
 }
