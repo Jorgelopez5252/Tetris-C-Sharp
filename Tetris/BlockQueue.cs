95cbec874b0bff030a940604de797a0f6c5f728d
Jorge A. Lopez <Jorgelopez5252@gmail.com>
2026-03-13 19:17:24 -0700

    block que added

---
+++ Tetris/BlockQueue.cs
@@ -0,0 +1,16 @@
+﻿namespace Tetris
+{
+    public class BlockQueue
+    {
+        private readonly Block[] block = new Block[]
+        {
+            new IBlock(),
+            new JBlock(),
+            new LBlock(),
+            new OBlock(),
+            new SBlock(),
+            new TBlock(),
+            new ZBlock(),
+        };
+    }
+}
